5bc45d398e9762b38d6fb885f138fb7a15f4a09f
nei.hash <ftthalmhdi@gmail.com>
2026-05-31 16:37:40 +0700

    Add Gaussian elimination for 4x4 matrices
    Implement Gaussian elimination for 4x4 matrices with back substitution.

--- main.py
+++ main.py
@@ -33,8 +33,43 @@ def gauss_ordo_2():
 def gauss_ordo_3():
   pass
 
-def gauss_ordo_4():
-  pass
+def matriks_4(matriks):
+    # copy martrix dan dapatkan panjang matrix nya
+    A = [row[:] for row in matriks]
+    n = len(A)
+    
+    # eliminasi maju untuk seitap baris
+    for k in range(n):
+        # cari nilai pivot nya
+        max_row = k
+        for r in range(k + 1, n):
+            if abs(A[r][k]) > abs(A[max_row][k]):
+                max_row = r
+                
+        # Jika baris dengan nilai terbesar bukan baris k, lakukan pertukaran
+        if max_row != k:
+            A[k], A[max_row] = A[max_row], A[k]
+        
+        # jika elemen diagonal nya tetep 0 maka tidak memilik solusi unik
+        if abs(A[k][k]) < 1e-12: 
+            raise ValueError("Sistem tidak memiliki solusi unik (Matriks Singular).")
+
+        # Proses eliminasi baris di bawahnya
+        for i in range(k + 1, n):
+            faktor = A[i][k] / A[k][k]
+            for j in range(k, n + 1):
+                A[i][j] -= faktor * A[k][j]
+                
+    # implementasi back subsitution
+    x = [0] * n
+    for i in range(n - 1, -1, -1):
+        sum_ax = 0
+        for j in range(i + 1, n):
+            sum_ax += A[i][j] * x[j]
+            
+        x[i] = (A[i][n] - sum_ax) / A[i][i]
+        
+    return x
 
 
 def main():
@@ -52,4 +87,5 @@ def main():
     elif pilihan == 3:
         gauss_ordo_3()
     elif pilihan == 4:
-        gauss_ordo_4()
+       ans = matriks_4(ordo_4)
+       print(f"Solusi dari matrikx ordo 4 x1 = {ans[0}, x2 = {ans[1]}, x3 = {ans[2]}, x4 = {ans[3}")
