3b678dfe196b2d9ed35e33ad4b0cf2a32dbebe4b
Aripe-netizen <ahmadarifkurniawan44@gmail.com>
2026-06-21 21:59:59 +0700

    Menambahkan eliminasi gauss ordo 3

--- main.py
+++ main.py
@@ -30,8 +30,49 @@ def gauss_ordo_1():
 def gauss_ordo_2():
   pass
 
-def gauss_ordo_3():
-  pass
+def gauss_ordo_3(m):
+    # Eliminasi maju
+    for i in range(3):
+        if m[i][i] == 0:
+            print("Pivot bernilai 0")
+            return
+
+        for j in range(i + 1, 3):
+            faktor = m[j][i] / m[i][i]
+
+            for k in range(4):
+                m[j][k] = m[j][k] - faktor * m[i][k]
+
+    print("Matriks setelah eliminasi:")
+    for baris in m:
+        print(baris)
+
+    # Substitusi mundur
+    z = m[2][3] / m[2][2]
+
+    y = (m[1][3] - m[1][2] * z) / m[1][1]
+
+    x = (m[0][3] - m[0][2] * z - m[0][1] * y) / m[0][0]
+
+    print("\nHasil:")
+    print("x =", x)
+    print("y =", y)
+    print("z =", z)
+
+
+# Input matriks augmented 3x4
+matrix = []
+
+print("Masukkan elemen matriks augmented 3x4:")
+
+for i in range(3):
+    baris = []
+    for j in range(4):
+        nilai = float(input(f"Baris {i+1} Kolom {j+1}: "))
+        baris.append(nilai)
+    matrix.append(baris)
+
+gauss_ordo_3(matrix)
 
 def gauss_ordo_4():
   pass
